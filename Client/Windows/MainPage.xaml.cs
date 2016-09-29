04323e1f0af2f3333071dee0109b8b4a43f0e4c1
Chaozong Sun <chaome545@163.com>
2016-09-29 10:18:51 +0800

    Update the videoGuest UI, it's now a very simple form with only a video frame.

--- Client/Windows/MainPage.xaml.cs
+++ Client/Windows/MainPage.xaml.cs
@@ -1,91 +1,30 @@
-// Copyright (c) Microsoft. All rights reserved.
-
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.ApplicationSettings;
-using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-using SDKTemplate.Common;
 
 namespace SDKTemplate
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         public static MainPage Current;
 
         public MainPage()
         {
-            this.InitializeComponent();
-            SampleTitle.Text = FEATURE_NAME;
-
-            // This is a static public property that allows downstream pages to get a handle to the MainPage instance
-            // in order to call methods that are in this class.
+            InitializeComponent();  
             Current = this;
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
-            // Populate the scenario list from the SampleConfiguration.cs file
-            ScenarioControl.ItemsSource = scenarios;
-
-            // If we have saved state return to the previously selected scenario  
-            if (SuspensionManager.SessionState.ContainsKey("SelectedScenarioIndex"))
-            {
-                ScenarioControl.SelectedIndex = Convert.ToInt32(SuspensionManager.SessionState["SelectedScenarioIndex"]);
-                ScenarioControl.ScrollIntoView(ScenarioControl.SelectedItem);   
-            }
-            else
-            {
-                ScenarioControl.SelectedIndex = 0;
-            }
-
-        }
-
-        /// <summary>
-        /// Called whenever the user changes selection in the scenarios list.  This method will navigate to the respective
-        /// sample scenario page.
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void ScenarioControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            // Clear the status block when navigating scenarios.
-            NotifyUser(String.Empty, NotifyType.StatusMessage);
-
-            ListBox scenarioListBox = sender as ListBox;
-            Scenario s = scenarioListBox.SelectedItem as Scenario;
-            if (s != null)
-            {
-                SuspensionManager.SessionState["SelectedScenarioIndex"] = scenarioListBox.SelectedIndex;
-                ScenarioFrame.Navigate(s.ClassType);
-            }
-                        
-        }
-
-        public List<Scenario> Scenarios
-        {
-            get { return this.scenarios; }
+            ScenarioFrame.Navigate(typeof(SimpleCommunication.VideoGuest));
         }
 
         /// <summary>
         /// Used to display messages to the user
         /// </summary>
-        /// <param name="strMessage"></param>
-        /// <param name="type"></param>
         public void NotifyUser(string strMessage, NotifyType type)
         {
             switch (type)
@@ -100,13 +39,13 @@ namespace SDKTemplate
             StatusBlock.Text = strMessage;
 
             // Collapse the StatusBlock if it has no text to conserve real estate.
-            if (StatusBlock.Text != String.Empty)
+            if (StatusBlock.Text != string.Empty)
             {
-                StatusBorder.Visibility = Windows.UI.Xaml.Visibility.Visible;
+                StatusBorder.Visibility = Visibility.Visible;
             }
             else
             {
-                StatusBorder.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+                StatusBorder.Visibility = Visibility.Collapsed;
             }
         }
 
@@ -114,7 +53,6 @@ namespace SDKTemplate
         {
             await Windows.System.Launcher.LaunchUriAsync(new Uri(((HyperlinkButton)sender).Tag.ToString()));
         }
-
     }
 
     public enum NotifyType
@@ -122,19 +60,4 @@ namespace SDKTemplate
         StatusMessage,
         ErrorMessage
     };
-
-    public class ScenarioBindingConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, string language)
-        {
-            Scenario s = value as Scenario;
-            return (MainPage.Current.Scenarios.IndexOf(s) + 1) + ") " + s.Title;
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, string language)
-        {
-            return true;
-        }
-    }
-
 }
--- Client/Windows/VideoGuest.xaml.cs
+++ Client/Windows/VideoGuest.xaml.cs
@@ -1,32 +1,17 @@
-﻿//*********************************************************
-//
-// Copyright (c) Microsoft. All rights reserved.
-// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
-// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
-// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
-// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
-//
-//*********************************************************
-
-using Windows.UI.Xaml;
+﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using SDKTemplate;
 using System;
 
 namespace SimpleCommunication
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class VideoGuest : Page
     {
-        // A pointer back to the main page.  This is needed if you want to call methods in MainPage such
-        // as NotifyUser()
         MainPage rootPage = MainPage.Current;
         
         public VideoGuest()
         {
-            this.InitializeComponent();
+            InitializeComponent();
             rootPage.EnsureMediaExtensionManager();
             RemoteVideo.MediaFailed += RemoteVideo_MediaFailed;
         }
@@ -35,26 +20,17 @@ namespace SimpleCommunication
         {
             rootPage.NotifyUser("RemoteVideo_MediaFailed:" + e.ErrorMessage, NotifyType.ErrorMessage);
         }
-
-
-        /// <summary>
-        /// This is the click handler for the 'Default' button.  You would replace this with your own handler
-        /// if you have a button or buttons on this page.
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
+        
         private void Call_Click(object sender, RoutedEventArgs e)
         {
             Button b = sender as Button;
             var address = HostName.Text;
 
-            if (b != null && !String.IsNullOrEmpty(address))
+            if (b != null && !string.IsNullOrEmpty(address))
             { 
                 RemoteVideo.Source = new Uri("stsp://" + address); 
                 rootPage.NotifyUser("Initiating connection... Please wait.", NotifyType.StatusMessage);
             }
-        }
- 
- 
+        } 
     }
 }
