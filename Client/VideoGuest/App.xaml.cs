345df4e83daaa9f9742409a35b252270af2a2b4b
Chaozong Sun <chaome545@163.com>
2016-10-10 11:18:48 +0800

    Add active protocol for VideoGuest.

--- Client/VideoGuest/App.xaml.cs
+++ Client/VideoGuest/App.xaml.cs
@@ -1,21 +1,8 @@
-// Copyright (c) Microsoft. All rights reserved.
-
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
 using SDKTemplate.Common;
 using SimpleCommunication;
@@ -36,58 +23,121 @@ namespace SDKTemplate
             InitializeComponent();
             Suspending += OnSuspending;
         }
-
-        /// <summary>
-        /// Invoked when the application is launched normally by the end user.  Other entry points
-        /// will be used such as when the application is launched to open a specific file.
-        /// </summary>
-        /// <param name="e">Details about the launch request and process.</param>
-        protected override async void OnLaunched(LaunchActivatedEventArgs e)
+        
+        private Frame CreateRootFrame()
         {
             Frame rootFrame = Window.Current.Content as Frame;
 
-            // Do not repeat app initialization when the Window already has content,
-            // just ensure that the window is active
-
+            // Do not repeat app initialization when the Window already has content, 
+            // just ensure that the window is active 
             if (rootFrame == null)
             {
-                // Create a Frame to act as the navigation context and navigate to the first page
+                // Create a Frame to act as the navigation context and navigate to the first page 
                 rootFrame = new Frame();
 
-                // Set the default language
+                // Set the default language 
                 rootFrame.Language = Windows.Globalization.ApplicationLanguages.Languages[0];
-                
-                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
-                {
-                    // Restore the saved session state only when appropriate
-                    try
-                    {
-                        await SuspensionManager.RestoreAsync();
-                    }
-                    catch (SuspensionManagerException)
-                    {
-                        //Something went wrong restoring state.
-                        //Assume there is no state and continue
-                    }
-                }
+                rootFrame.NavigationFailed += OnNavigationFailed;
 
-                // Place the frame in the current Window
+                SuspensionManager.RegisterFrame(rootFrame, "AppFrame");
+
+                // Place the frame in the current Window 
                 Window.Current.Content = rootFrame;
             }
 
+            return rootFrame;
+        }
+        
+        /// <summary> 
+        /// Invoked when Navigation to a certain page fails 
+        /// </summary> 
+        /// <param name="sender">The Frame which failed navigation</param> 
+        /// <param name="e">Details about the navigation failure</param> 
+        void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+        }
+
+        private async void RestoreStatus(ApplicationExecutionState previousExecutionState)
+        {
+            // Do not repeat app initialization when the Window already has content, 
+            // just ensure that the window is active 
+            if (previousExecutionState == ApplicationExecutionState.Terminated)
+            {
+                // Restore the saved session state only when appropriate 
+                try
+                {
+                    await SuspensionManager.RestoreAsync();
+                }
+                catch (SuspensionManagerException)
+                {
+                    //Something went wrong restoring state. 
+                    //Assume there is no state and continue 
+                }
+            }
+        }
+
+        /// <summary> 
+        // Handle file activations. 
+        /// </summary> 
+        protected override void OnFileActivated(FileActivatedEventArgs e)
+        {
+            Frame rootFrame = CreateRootFrame();
+            RestoreStatus(e.PreviousExecutionState);
+
             if (rootFrame.Content == null)
             {
-                // When the navigation stack isn't restored navigate to the first page,
-                // configuring the new page by passing required information as a navigation
-                // parameter
-                if (!rootFrame.Navigate(typeof(VideoGuest), e.Arguments))
+                if (!rootFrame.Navigate(typeof(VideoGuest)))
                 {
                     throw new Exception("Failed to create initial page");
                 }
             }
 
-            // Ensure the current window is active
+            var p = rootFrame.Content as VideoGuest;
+ 
+            // Ensure the current window is active 
             Window.Current.Activate();
+        }
+
+        /// <summary> 
+        /// Invoked when the application is launched normally by the end user.  Other entry points 
+        /// will be used such as when the application is launched to open a specific file. 
+        /// </summary> 
+        /// <param name="e">Details about the launch request and process.</param> 
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        {
+            Frame rootFrame = CreateRootFrame();
+            RestoreStatus(e.PreviousExecutionState);
+
+            //MainPage is always in rootFrame so we don't have to worry about restoring the navigation state on resume 
+            rootFrame.Navigate(typeof(VideoGuest), e.Arguments);
+
+            // Ensure the current window is active 
+            Window.Current.Activate();
+        }
+
+        /// <summary> 
+        // Handle protocol activations and continuation activations. 
+        /// </summary> 
+        protected override void OnActivated(IActivatedEventArgs e)
+        {
+            if (e.Kind == ActivationKind.Protocol)
+            {
+                ProtocolActivatedEventArgs protocolArgs = e as ProtocolActivatedEventArgs;
+                Frame rootFrame = CreateRootFrame();
+                RestoreStatus(e.PreviousExecutionState);
+
+                if (rootFrame.Content == null)
+                {
+                    if (!rootFrame.Navigate(typeof(VideoGuest)))
+                    {
+                        throw new Exception("Failed to create initial page");
+                    }
+                } 
+
+                // Ensure the current window is active 
+                Window.Current.Activate();
+            }
         }
 
         /// <summary>
